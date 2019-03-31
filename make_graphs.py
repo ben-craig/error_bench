c26c67ee44b3a85e39d61aeaaccd116d38088014
Ben Craig <ben.craig@gmail.com>
2019-03-31 16:54:58 -0500

    Making intermediate echarts easier to work with

--- make_graphs.py
+++ make_graphs.py
@@ -3,17 +3,46 @@
 import re
 import os
 
+ORDINAL_MAPPING = {
+  "expected_struct" :            16,
+  "expected_val" :               15,
+  "noexcept_abort" :         10,
+  "ref_struct" :                 14,
+  "ref_val" :                    13,
+  "return_struct" :              12,
+  "return_val" :                 11,
+  "stripped.expected_struct" :   6,
+  "stripped.expected_val" :      5,
+  "stripped.ref_struct" :        4,
+  "stripped.ref_val" :           3,
+  "stripped.return_struct" :     2,
+  "stripped.return_val" :        1,
+  "stripped.abort" :         0,
+  "stripped.tls_error_struct" :  8,
+  "stripped.tls_error_val" :     7,
+  "abort" :                  9,
+  "throw_exception" :            21,
+  "throw_struct" :               20,
+  "throw_val" :                  19,
+  "tls_error_struct" :           18,
+  "tls_error_val" :              17,
+}
+
+
 class SeriesData(object):
     def __init__(self, elt_map):
         self.plat = elt_map["plat"]
-        self.ord = int(elt_map["Ord"])
         self.type = elt_map["type"]
+        self.ord = ORDINAL_MAPPING[self.type]
         self.eltMap = elt_map
         self.cmpTerm = elt_map["CmpTerm"]
         self.cost2ndNeutral = elt_map["Cost2ndNeutral"]
         self.cost1stError = elt_map["Cost1stError"]
         self.cost2ndError = elt_map["Cost2ndError"]
         self.cost1stCatch = elt_map["Cost1stCatch"]
+        if "Cost2ndCatch" not in elt_map:
+            print(elt_map)
+            raise elt_map
         self.cost2ndCatch = elt_map["Cost2ndCatch"]
 
 class SeriesTypeData(object):
@@ -29,9 +58,9 @@ def make_graphs(tsv_name, out_name):
         for line in fin:
             if first:
                 first = False
-                headers = line.split('\t')
+                headers = line.split(',')
                 continue
-            tmp = line.split('\t')
+            tmp = line.split(',')
             elt = {}
             for i in range(len(tmp)):
                 elt[headers[i]] = tmp[i]
@@ -42,7 +71,7 @@ def make_graphs(tsv_name, out_name):
                 data[seriesData.ord].plat_map[seriesData.plat] = seriesData
 
     with open(out_name, 'w') as fout:
-        spaces = "" #" "*8
+        spaces = "  "
 
         cases = [
             "CmpTerm",
@@ -54,7 +83,7 @@ def make_graphs(tsv_name, out_name):
         ]
         
         for case in cases:
-            fout.write(case + "\n\n")
+            fout.write("var data_" + case + " = [\n")
             first = True
             for ord in sorted(data.keys()):
                 if not first:
@@ -72,6 +101,7 @@ def make_graphs(tsv_name, out_name):
                     data[ord].plat_map["MSVC/x64"].eltMap[case] + "," +
                     data[ord].plat_map["MSVC/x86"].eltMap[case] + "]\n")
             fout.write(spaces + "}\n")
+            fout.write("];\n\n");
 
 def main():
     import argparse
