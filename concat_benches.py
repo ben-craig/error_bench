dc5a19065e6a1d214c828db37d54fe93d596e990
Ben Craig <ben.craig@gmail.com>
2019-07-28 17:50:23 -0500

    stuff runs, committing before a really long run

--- concat_benches.py
+++ concat_benches.py
@@ -2,16 +2,21 @@
 import sys
 import subprocess
 
+SIZES = [1,2,4,8,16]
+
 def main():
-    with open(sys.argv[1], 'wb') as fout:
-      with open(sys.argv[2], 'r') as fin:
-        for name in fin.read().split():
-            res = subprocess.check_output([name])
-            fout.write((name + ",").encode('utf-8'))
-            fout.write(res)
-            print(name)
+  with open(sys.argv[1], 'wb') as fout:
+    with open(sys.argv[2], 'r') as fin:
+      for name in fin.read().split():
+        res = subprocess.check_output([name])
+        idx = 0
+        for res_line in res.split():
+          fout.write((name + "," + str(SIZES[idx]) + ",").encode('utf-8'))
+          fout.write(res_line + "\n".encode('utf-8'))
+          idx = idx + 1
+        print(name)
 
 if __name__ == '__main__':
-    if sys.version_info[0] < 3:
-        raise Error("Must be using Python 3")
-    exit(main())
+  if sys.version_info[0] < 3:
+    raise Error("Must be using Python 3")
+  exit(main())
